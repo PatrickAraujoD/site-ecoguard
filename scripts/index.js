041ce5229b56241a99a2410822816af71e86af08
Vitor Gabriel <vitor.grs2004@gmail.com>
2023-11-03 12:08:53 -0300

    mapa ecopontos

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,64 @@
+let map;
+let popup = L.popup();
+let currentCoords = null;
 
-let h2 = document.querySelector('h2');
-var map;
-
-function success(pos){
-    console.log(pos.coords.latitude, pos.coords.longitude);
-   
-    
-    if (map === undefined) {
-        map = L.map('mapid').setView([pos.coords.latitude, pos.coords.longitude], 13);
-    } else {
-        map.remove();
-        map = L.map('mapid').setView([pos.coords.latitude, pos.coords.longitude], 13);
-    }
+function initializeMap(latitude, longitude) {
+    map = L.map('mapid').setView([latitude, longitude], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map)
-        .bindPopup('Eu estou aqui!')
-        .openPopup();
+    const marker = L.marker([latitude, longitude]).addTo(map);
+    marker.bindPopup('Eu estou aqui!').openPopup();
+
+    map.on('click', onMapClick);
 }
 
-function error(err){
+function success(pos) {
+    const { latitude, longitude } = pos.coords;
+
+    if (!map) {
+        initializeMap(latitude, longitude);
+    } else {
+        currentCoords = { latitude, longitude };
+    }
+}
+
+function error(err) {
     console.log(err);
 }
 
-var watchID = navigator.geolocation.watchPosition(success, error, {
+const watchID = navigator.geolocation.watchPosition(success, error, {
     enableHighAccuracy: true,
-    timeout: 5000
+    timeout: Infinity
 });
 
-//navigator.geolocation.clearWatch(watchID);
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent(`Você clicou no mapa em ${e.latlng.toString()}`)
+        .openOn(map);
+}
+
+function checkLocation() {
+    if (currentCoords !== null) {
+        navigator.geolocation.getCurrentPosition((pos) => {
+            const { latitude, longitude } = pos.coords;
+
+            const latDiff = Math.abs(latitude - currentCoords.latitude);
+            const lngDiff = Math.abs(longitude - currentCoords.longitude);
+
+            // Verificar se a diferença é significativa
+            if (latDiff > 0.0001 || lngDiff > 0.0001) {
+                map.setView([latitude, longitude], 13);
+                const marker = L.marker([latitude, longitude]).addTo(map);
+                marker.bindPopup('Eu estou aqui!').openPopup();
+                currentCoords = { latitude, longitude };
+            }
+        }, error, { enableHighAccuracy: true, timeout: 5000 });
+    }
+}
+
+// Verificar a localização a cada 30 segundos
+setInterval(checkLocation, 30000); // Intervalo de 30 segundos
