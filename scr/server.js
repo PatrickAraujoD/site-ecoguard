8f3b8d6d2f9143ad09a654d8f2a735d2cc4eb48c
Vitor Gabriel <vitor.grs2004@gmail.com>
2023-11-19 16:42:54 -0300

    node.js configurado

--- scr/server.js
+++ scr/server.js
@@ -1,39 +1,27 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const { Pool } = require('pg');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Configurar o pool do PostgreSQL
+// Configuração do pool do PostgreSQL (substitua com suas credenciais)
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  user: 'postgres',
+  host: 'localhost',
+  database: 'ecoguard',
+  password: '2023',
+  port: 5432,
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.json());
 
-// Rota para processar os dados do formulário
-app.post('/inserir-dados', async (req, res) => {
+// Rota para lidar com o envio de formulário
+app.post('/enviar-formulario', async (req, res) => {
   try {
     const { denuncia, data, relato, logradouro, complemento, cidade, bairro, descricaoLocal, contatos } = req.body;
-
-    // Conectar ao banco de dados
-    const client = await pool.connect();
-
-    // Inserir dados na tabela
-    await client.query(
-      'INSERT INTO formulario_denuncia.denuncias (tipo_de_denuncia, data_do_ocorrido, relato, logradouro, complemento, cidade, bairro, descricao_do_local, contato) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
-      [denuncia, data, relato, logradouro, complemento, cidade, bairro, descricaoLocal, contatos]
-    );
-
-    res.status(201).json({ message: 'Registro inserido com sucesso!' });
-
-    // Liberar conexão
-    client.release();
+    const result = await pool.query('INSERT INTO formulario_denuncia.denuncias (tipo_de_denuncia, data_do_ocorrido, relato, logradouro, complemento, cidade, bairro, descricao_do_local, contato) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *', [denuncia, data, relato, logradouro, complemento, cidade, bairro, descricaoLocal, contatos]);
+    //Envie uma resposta com os dados inseridos
+    res.json(result.rows[0]);
   } catch (error) {
     console.error('Erro ao inserir registro:', error);
     res.status(500).json({ error: 'Erro interno do servidor' });
