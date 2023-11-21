c3196c679b050837b5b1b5e3817e25e8038eadf5
Vitor Gabriel <vitor.grs2004@gmail.com>
2023-11-21 01:58:38 -0300

    banco de dados conect

---
+++ scr/server.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+
+async function inserirResposta(denuncia, data, relato, logradouro, complemento, cidade, bairro, descricaoLocal, contatos) {
+  const query = 'INSERT INTO denuncias (id, tipo_de_denuncia, data_do_ocorrido, relato, logradouro, complemento, cidade, bairro, descricao_do_local, contato) VALUES (DEFAULT,$1, $2, $3, $4, $5, $6, $7, $8, $9)';
+  const values = [denuncia, data, relato, logradouro, complemento, cidade, bairro, descricaoLocal, contatos];
+
+  try {
+    const result = await pool.query(query, values);
+    console.log('Resposta inserida com sucesso:', result);
+  } catch (error) {
+    console.error('Erro ao inserir resposta:', error);
+  }
+}
+
